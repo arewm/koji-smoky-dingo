ec5ad48aff2290dc456e5bdd2d6929b34aa97478
Christopher O'Brien <cobrien@redhat.com>
2020-09-20 16:18:12 -0400

    deploying sphinx update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.1  # eg. 1.0.0',
+    VERSION: '0.9.1',
     LANGUAGE: 'English',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '',
